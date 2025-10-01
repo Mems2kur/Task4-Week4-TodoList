30c6e50666da38e441d6732458dbd873fe8d27ce
Mems_2kur <tukurmaimuna15@gmail.com>
2025-10-02 00:45:24 +0100

    Initial commit - Todo List project

---
+++ script.js
@@ -0,0 +1,45 @@
+const input = document.querySelector("#input");
+const btn = document.querySelector(".btn");
+const itemsList = document.querySelector("#items-list");
+
+btn.addEventListener("click", () => {
+  const InputTask = input.value.trim();
+  if (InputTask !== "") {
+    const Li = document.createElement("li");
+
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("check");
+    Li.appendChild(checkbox);
+
+    
+    const span = document.createElement("span");
+    span.textContent = InputTask;
+    Li.appendChild(span);
+
+    const icon = document.createElement("i");
+    icon.className = "fa-solid fa-delete-left";
+    icon.style.cursor = "pointer";
+    icon.style.color ="white"
+    Li.appendChild(icon);
+
+
+    icon.addEventListener("click", () => {
+      Li.remove();
+    });
+
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        span.style.textDecoration = "line-through";
+        span.style.color = "#aaa";
+      } else {
+        span.style.textDecoration = "none";
+        span.style.color = "";
+      }
+    });
+
+    itemsList.appendChild(Li);
+    input.value = "";
+  }
+});
